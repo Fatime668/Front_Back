df5e885fcb554393c702d5725c2ad7d5008a76dc
Fatime668 <tu7dkjdue@code.edu.az>
2022-06-13 11:46:17 +0400

    Project

---
+++ project/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Logo { get; set; }
+    }
+}
